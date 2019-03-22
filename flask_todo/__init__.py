a56a5c3a8edeb8d4f6bd321a5eafc11751e46c10
Zach Lehman <zalehman182@stevenscollege.edu>
2019-03-22 10:03:09 -0400

    Setup for the Todo app
    - Create each route and page for the app
    - Create test section for actual app
    - App is working with the least amount of structure

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, make_response, render_template
 
-def create_app():
-    app = Flask(__name__)
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
     @app.route('/')
     def index():
-        return 'Hello, World!'
+        return render_template('index.html')
+
+    @app.route('/create')
+    def create():
+        return render_template('create.html')
+
+    @app.route('/update')
+    def update():
+        return render_template('update.html')
 
     return app
