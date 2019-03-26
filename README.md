8bff4cd50a9b8cb467d248f9691d36c13a7a540c
Zach Lehman <zalehman182@stevenscollege.edu>
2019-03-26 11:07:06 -0400

    Update all routes and the CSS for those routes
    - Create page can input task and adds to file and seen on index page
    - Update page can input completed task and it will update of index page
    - create a way to see the file on the home page
    - add CSS

---
+++ README.md
@@ -0,0 +1,17 @@
+# This is a Todo App made from Flask
+
+1. You can first fork then clone the repository
+
+2. Then you can make you own virtual environment with     python3 -m venv <name>
+
+3. Activate the environment     source venv/bin/activate
+
+4. Then write     pip install -e .
+
+5. Then write     pip install -e '.[test]'
+
+6. export FLASK_APP=flask_todo
+
+7. export FLASK_ENV=development
+
+8. Then you can flask run
--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -14,27 +14,56 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+
     @app.route('/')
     def index():
-        return render_template('index.html')
+        post = open('todo.txt', 'r+')
+        file = post.read()
+        post.close()
+        return render_template('index.html', post=file)
+
+
 
     @app.route('/create', methods=['GET', 'POST'])
     def create():
         if request.method == 'GET':
             return render_template('create.html')
         elif request.method == 'POST':
-            lists = open("todos.txt", "a")
+            lists = open("todo.txt", "a")
             todo = request.form['todo']
+            posts = []
             if not todo:
                 return 'You need to add a Todo'
-            lists.write(todo), lists.write(' '),  lists.write(str(datetime.datetime.now())), lists.write(' '), lists.write('Not Completed'), lists.write('\n')
+            posts.append(lists.write(todo)),
+            posts.append(lists.write(' ')), posts.append(lists.write(str(datetime.datetime.now()))), posts.append(lists.write(' ')),
+            posts.append(lists.write('Not Completed')),
+            posts.append(lists.write('\n'))
             lists.close()
 
 
-        return render_template('create.html', todo=todo)
+        return render_template('create.html', todo=todo, posts=posts)
 
-    @app.route('/update')
+
+    @app.route('/update', methods=['GET', 'POST'])
     def update():
-        return render_template('update.html')
+        if request.method == 'GET':
+            return render_template('update.html')
+        elif request.method == 'POST':
+            lists = open("todo.txt", "r+")
+            todo = request.form['todo']
+            reading = lists.read()
+            if todo == '':
+                lists.close()
+                return render_template('update.html')
+            else:
+                lists = open("todo.txt", "r+")
+                file_text = lists.readlines()
+                lists.seek(0)
+                for i in file_text:
+                    if not todo in i:
+                        lists.write(i)
+                lists.truncate()
+                lists.close()
+        return render_template('update.html', todo=todo)
 
     return app
